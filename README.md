337e848d4c43bcb3c2f30e87d1c2617696ef2932
David Lindström <david.lindstrom@percepio.com>
2024-05-04 02:10:40 +0200

    Initial example mod import

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # GTNH-Web-Map-Client
-Access to GWM maps from the client
+Access to GWM maps from the client. Not currently in a usable state.
---
+++ src/main/java/com/gtnewhorizons/gwmclient/Config.java
@@ -0,0 +1,18 @@
+package com.gtnewhorizons.gwmclient;
+
+import java.io.File;
+
+import net.minecraftforge.common.config.Configuration;
+
+public class Config {
+
+
+    public static void synchronizeConfiguration(File configFile) {
+        Configuration configuration = new Configuration(configFile);
+
+
+        if (configuration.hasChanged()) {
+            configuration.save();
+        }
+    }
+}
